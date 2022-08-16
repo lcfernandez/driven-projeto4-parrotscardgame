5f259483728cdfae02bceb64317240afbe87f70b
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-16 11:56:41 -0300

    Distribuição da quantidade de cartas requeridas

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const main = document.querySelector("main");
 let quantity = 0;
 
 while ((quantity < 4) || (quantity > 14) || (quantity %2 !== 0)) {
   quantity = prompt("Com quantas cartas você quer jogar?");
+}
+
+for (let i = 0; i < quantity; i++) {
+  main.innerHTML += `<div class="card" id="${i}"></div>`;
 }
