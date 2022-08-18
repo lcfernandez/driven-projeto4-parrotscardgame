505c8665edbed0ca6d5cc614bd4ae65dfcf198d7
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-18 11:39:04 -0300

    Inclusão de outras faces e lógica para criação de pares

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
-const main = document.querySelector("main");
-let quantity = 0;
-const array = [];
+/* variables */
 
-while ((quantity < 4) || (quantity > 14) || (quantity %2 !== 0)) {
-  quantity = prompt("Com quantas cartas você quer jogar?");
+const main = document.querySelector("main");
+const faces = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"]
+const cards = [];
+let amount = 0;
+
+
+/* functions */
+
+function choose(card) {
+  card.querySelector(".front-face").classList.add("flip");
+  card.querySelector(".back-face").classList.add("hold");
 }
 
 function comparator() { 
 	return Math.random() - 0.5; 
 }
 
-for (let i = 0; i < quantity; i++) {
-  array.push(`<div class="card" onclick="choose(this)"><div class="front-face face"><img src="../images/front.png" /></div><div class="back-face face"><img src="../images/metalparrot.gif" /></div></div>`);
+
+/* asking how many cards the player wants */
+while ((amount < 4) || (amount > 14) || (amount %2 !== 0)) {
+  amount = prompt("Com quantas cartas você quer jogar?");
 }
 
-array.sort(comparator);
-
-for (let i = 0; i < quantity; i++) {
-  main.innerHTML += array[i];
+/* sorting the faces and creating cards */
+faces.sort(comparator);
+for (let i = 0; i < amount/2; i++) {
+  for (let j = 0; j < 2; j++) {
+    cards.push(`<div class="card" onclick="choose(this)"><div class="front-face face"><img src="../images/front.png" /></div><div class="back-face face"><img src="../images/${faces[i]}parrot.gif" /></div></div>`);
+  }
 }
 
-function choose(card) {
-  card.querySelector(".front-face").classList.add("flip");
-  card.querySelector(".back-face").classList.add("hold");
+/* sorting the cards and including into de game */
+cards.sort(comparator);
+for (let i = 0; i < amount; i++) {
+  main.innerHTML += cards[i];
 }
