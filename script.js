9b9774c4f91081aadcd3c8ddcad274e89c11819a
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-18 15:53:00 -0300

    Finalização da lógica geral do jogo

--- script.js
+++ script.js
@@ -4,17 +4,70 @@ const main = document.querySelector("main");
 const faces = ["bobross", "explody", "fiesta", "metal", "revertit", "triplets", "unicorn"]
 const cards = [];
 let amount = 0;
+let moves = 0;
+let matchingPairs = 0;
+let lastChosen;
 
 
 /* functions */
 
 function choose(card) {
-  card.querySelector(".front-face").classList.add("flip");
-  card.querySelector(".back-face").classList.add("hold");
+  flip(card);
+  moves++;
+
+  if (moves == 0 || (moves % 2 === 1)) {
+    lastChosen = card;
+  } else {
+    check(card);
+  }
+}
+
+function check(card) {
+  unFreeze("F");
+
+  if (card.querySelector(".back-face img").src === lastChosen.querySelector(".back-face img").src) {
+    matchingPairs++;
+    lastChosen.classList.add("match");
+    card.classList.add("match");
+
+    if (matchingPairs === amount / 2) {
+      alert(`Você ganhou em ${moves} jogadas!`);
+    } else {
+      unFreeze("U");
+    }
+  } else {
+    setTimeout(function (){
+      flip(lastChosen);
+      flip(card);
+      unFreeze("U");
+      
+      lastChosen = undefined;
+    }, 1000);
+  }
 }
 
 function comparator() { 
 	return Math.random() - 0.5; 
+}
+
+function flip(card) {
+  card.querySelector(".front-face").classList.toggle("turn");
+  card.querySelector(".back-face").classList.toggle("hold");
+}
+
+function unFreeze(option) {
+  const cards = document.querySelectorAll(".card:not(.match)");
+  if (option === "U") {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].setAttribute("onclick", "choose(this)");
+    }
+  } else if(option === "F") {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].removeAttribute("onclick");
+    }
+  } else {
+    return;
+  }
 }
 
 
@@ -25,13 +78,13 @@ while ((amount < 4) || (amount > 14) || (amount %2 !== 0)) {
 
 /* sorting the faces and creating cards */
 faces.sort(comparator);
-for (let i = 0; i < amount/2; i++) {
+for (let i = 0; i < amount / 2; i++) {
   for (let j = 0; j < 2; j++) {
     cards.push(`<div class="card" onclick="choose(this)"><div class="front-face face"><img src="../images/front.png" /></div><div class="back-face face"><img src="../images/${faces[i]}parrot.gif" /></div></div>`);
   }
 }
 
-/* sorting the cards and including into de game */
+/* sorting the cards and including into the page */
 cards.sort(comparator);
 for (let i = 0; i < amount; i++) {
   main.innerHTML += cards[i];
