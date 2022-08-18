57fb7eb5cc3888aab23d1e0d5a26fd474d2cfab8
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-18 11:04:14 -0300

    Ajustes e implementações nas cartas
    - Criação das cartas numa array;
    - Embaralhamento das cartas;
    - Virada das cartas;
    - Inclusão de animação para testes.

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 const main = document.querySelector("main");
 let quantity = 0;
+const array = [];
 
 while ((quantity < 4) || (quantity > 14) || (quantity %2 !== 0)) {
   quantity = prompt("Com quantas cartas você quer jogar?");
 }
 
+function comparator() { 
+	return Math.random() - 0.5; 
+}
+
 for (let i = 0; i < quantity; i++) {
-  main.innerHTML += `<div class="card" id="${i}"></div>`;
+  array.push(`<div class="card" onclick="choose(this)"><div class="front-face face"><img src="../images/front.png" /></div><div class="back-face face"><img src="../images/metalparrot.gif" /></div></div>`);
+}
+
+array.sort(comparator);
+
+for (let i = 0; i < quantity; i++) {
+  main.innerHTML += array[i];
+}
+
+function choose(card) {
+  card.querySelector(".front-face").classList.add("flip");
+  card.querySelector(".back-face").classList.add("hold");
 }
