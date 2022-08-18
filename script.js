b4bb9a22b14e3bebd3517b5b29ae1ac20ee2aabb
lcfernandez <lcfernandez@cos.ufrj.br>
2022-08-18 16:27:42 -0300

    Ajuste de identação

--- script.js
+++ script.js
@@ -12,80 +12,80 @@ let lastChosen;
 /* functions */
 
 function choose(card) {
-  flip(card);
-  moves++;
+    flip(card);
+    moves++;
 
-  if (moves == 0 || (moves % 2 === 1)) {
-    lastChosen = card;
-  } else {
-    check(card);
-  }
+    if (moves == 0 || (moves % 2 === 1)) {
+        lastChosen = card;
+    } else {
+        check(card);
+    }
 }
 
 function check(card) {
-  unFreeze("F");
+    unFreeze("F");
 
-  if (card.querySelector(".back-face img").src === lastChosen.querySelector(".back-face img").src) {
-    matchingPairs++;
-    lastChosen.classList.add("match");
-    card.classList.add("match");
-
-    if (matchingPairs === amount / 2) {
-      alert(`Você ganhou em ${moves} jogadas!`);
+    if (card.querySelector(".back-face img").src === lastChosen.querySelector(".back-face img").src) {
+        matchingPairs++;
+        lastChosen.classList.add("match");
+        card.classList.add("match");
+        
+        if (matchingPairs === amount / 2) {
+            alert(`Você ganhou em ${moves} jogadas!`);
+        } else {
+            unFreeze("U");
+        }
     } else {
-      unFreeze("U");
+        setTimeout(function () {
+            flip(lastChosen);
+            flip(card);
+            unFreeze("U");
+            
+            lastChosen = undefined;
+        }, 1000);
     }
-  } else {
-    setTimeout(function (){
-      flip(lastChosen);
-      flip(card);
-      unFreeze("U");
-      
-      lastChosen = undefined;
-    }, 1000);
-  }
 }
 
 function comparator() { 
-	return Math.random() - 0.5; 
+    return Math.random() - 0.5; 
 }
 
 function flip(card) {
-  card.querySelector(".front-face").classList.toggle("turn");
-  card.querySelector(".back-face").classList.toggle("hold");
+    card.querySelector(".front-face").classList.toggle("turn");
+    card.querySelector(".back-face").classList.toggle("hold");
 }
 
 function unFreeze(option) {
-  const cards = document.querySelectorAll(".card:not(.match)");
-  if (option === "U") {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].setAttribute("onclick", "choose(this)");
+    const cards = document.querySelectorAll(".card:not(.match)");
+    if (option === "U") {
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].setAttribute("onclick", "choose(this)");
+        }
+    } else if (option === "F") {
+        for (let i = 0; i < cards.length; i++) {
+            cards[i].removeAttribute("onclick");
+        }
+    } else {
+        return;
     }
-  } else if(option === "F") {
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].removeAttribute("onclick");
-    }
-  } else {
-    return;
-  }
 }
 
 
 /* asking how many cards the player wants */
 while ((amount < 4) || (amount > 14) || (amount %2 !== 0)) {
-  amount = prompt("Com quantas cartas você quer jogar?");
+    amount = prompt("Com quantas cartas você quer jogar?");
 }
 
 /* sorting the faces and creating cards */
 faces.sort(comparator);
 for (let i = 0; i < amount / 2; i++) {
-  for (let j = 0; j < 2; j++) {
-    cards.push(`<div class="card" onclick="choose(this)"><div class="front-face face"><img src="../images/front.png" /></div><div class="back-face face"><img src="../images/${faces[i]}parrot.gif" /></div></div>`);
-  }
+    for (let j = 0; j < 2; j++) {
+        cards.push(`<div class="card" onclick="choose(this)"><div class="front-face face"><img src="../images/front.png" /></div><div class="back-face face"><img src="../images/${faces[i]}parrot.gif" /></div></div>`);
+    }
 }
 
 /* sorting the cards and including into the page */
 cards.sort(comparator);
 for (let i = 0; i < amount; i++) {
-  main.innerHTML += cards[i];
+    main.innerHTML += cards[i];
 }
